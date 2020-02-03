c1c63815ecb4a94c1836761a8aaf4759069ed1df
laundrydirty <a.sroller@seznam.cz>
2020-02-03 21:58:56 +0100

    3

--- trojka.py
+++ trojka.py
@@ -1,13 +1,17 @@
 #du3
+import json
+from pathlib import Path
 
 
+with open("input.geojson","r",encoding="utf-8") as f:
+    fjutrs=json.load(f)
 
 mypoints=[]
 mylinestrings=[]
 mypolygons=[]
 
 if fjutrs['features']['geometry']['type']=='Point':
-
+    fjutrs['features']['properties']['filepath']=pathlib(fjutrs)
     mypoints.append()
 elif fjutrs['features']['geometry']['type']=='Linestring':
 
@@ -19,3 +23,11 @@ else:
     print(chyba)
 
 
+with open("points.geojson","w",encoding="utf-8") as f:
+    json.dump(mypoints,f,indent=2,ensure_ascii=False)
+
+with open("linestrings.geojson","w",encoding="utf-8") as f:
+    json.dump(mylinestrings,f,indent=2,ensure_ascii=False)
+
+with open("polygons.geojson","w",encoding="utf-8") as f:
+    json.dump(mypolygons,f,indent=2,ensure_ascii=False)
