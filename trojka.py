5a29b96aaa8f4fa8902e9d7202c2f19074235045
laundrydirty <a.sroller@seznam.cz>
2020-02-03 18:34:30 +0100

    2

--- trojka.py
+++ trojka.py
@@ -1,1 +1,21 @@
 #du3
+
+
+
+points=[]
+linestring=[]
+polygon=[]
+
+if fjutrs['features']['geometry']['type']=='Point':
+
+    mypoints.append()
+elif fjutrs['features']['geometry']['type']=='Linestring':
+
+    mylinestrings.append()
+elif fjutrs['features']['geometry']['type']=='Polygon':
+
+    mypolygons.append()
+else:
+    print(chyba)
+
+
