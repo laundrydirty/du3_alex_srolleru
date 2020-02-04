2c62f42233473a8802d440ae20f567c1d07e1ff6
laundrydirty <a.sroller@seznam.cz>
2020-02-04 16:31:36 +0100

    funkcni bez komentaru

--- trojka.py
+++ trojka.py
@@ -1,38 +1,40 @@
-import json,os,sys
+import json,sys
 from pathlib import Path
 
 mypoints=[]
 mylinestrings=[]
 mypolygons=[]
 
-#chybi vyjimky a json format
-
-for filepath in Path(sys.argv[1]).rglob('**/*.geojson'):
-    with open(filepath, "r", encoding="utf-8") as f:
+extensions=['*.geojson','*.json']
+for e in extensions:
+    for filepath in Path(sys.argv[1]).rglob(e):
         try:
-            fjutrs = json.load(f)
-            print(filepath)
-            for pod in fjutrs['features']:
-                if pod['geometry']['type'] == 'Point':
-                    pod['filepath'] = str(filepath)
-                    mypoints.append(pod)
-                elif pod['geometry']['type'] == 'Linestring':
-                    pod['filepath'] = str(filepath)
-                    mylinestrings.append(pod)
-                elif pod['geometry']['type'] == 'Polygon':
-                    pod['filepath'] = str(filepath)
-                    mypolygons.append(pod)
-                else:
-                    pass
-        except json.JSONDecodeError:
-            print('invalid JSON format: ',filepath)
-
+            with open(filepath, "r", encoding="utf-8") as f:
+                try:
+                    fjutrs = json.load(f)
+                    print(filepath)
+                    for pod in fjutrs['features']:
+                        if pod['geometry']['type'] == 'Point':
+                            pod['filepath'] = str(filepath)
+                            mypoints.append(pod)
+                        elif pod['geometry']['type'] == 'LineString':
+                            pod['filepath'] = str(filepath)
+                            mylinestrings.append(pod)
+                        elif pod['geometry']['type'] == 'Polygon':
+                            pod['filepath'] = str(filepath)
+                            mypolygons.append(pod)
+                        else:
+                            pass
+                except json.JSONDecodeError:
+                    print('invalid JSON format: ', filepath)
+        except PermissionError:
+            print('invalid JSON format: ', filepath)
 
 
 with open("points.geojson","w",encoding="utf-8") as p:
     json.dump(mypoints,p,indent=2,ensure_ascii=False)
 
-with open("linestrings.geojson","w",encoding="utf-8") as l:
+with open("lines.geojson","w",encoding="utf-8") as l:
     json.dump(mylinestrings,l,indent=2,ensure_ascii=False)
 
 with open("polygons.geojson","w",encoding="utf-8") as po:
