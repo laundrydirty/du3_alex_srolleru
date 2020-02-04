35f0edc26420c0b3f92427731ef95938ec90cb1b
laundrydirty <a.sroller@seznam.cz>
2020-02-04 14:12:20 +0100

    6

--- trojka.py
+++ trojka.py
@@ -1,33 +1,35 @@
-#du3
 import json,os
-
-
-with open("input.geojson","r",encoding="utf-8") as f:
-    fjutrs=json.load(f)
+from pathlib import path
 
 mypoints=[]
 mylinestrings=[]
 mypolygons=[]
 
-for pod in fjutrs['features']:
-    if pod['geometry']['type']=='Point':
-        pod['filepath']=os.path.abspath('fjutrs')
-        mypoints.append(pod)
-    elif pod['geometry']['type']=='Linestring':
-        pod['filepath'] = os.path.abspath('fjutrs')
-        mylinestrings.append(pod)
-    elif pod['geometry']['type']=='Polygon':
-        pod['filepath'] = os.path.abspath('fjutrs')
-        mypolygons.append(pod)
-    else:
-        print(chyba)
+for filepath in path(sys.argv[1]).rglob('**/*.geojson'):
+    filename=os.path.basename(filepath)
+    with open(filename, "r", encoding="utf-8") as f:
+        fjutrs = json.load(f)
 
 
-with open("points.geojson","w",encoding="utf-8") as f:
-    json.dump(mypoints,f,indent=2,ensure_ascii=False)
+    for pod in fjutrs['features']:
+        if pod['geometry']['type']=='Point':
+            pod['filepath']=os.path.abspath('fjutrs')
+            mypoints.append(pod)
+        elif pod['geometry']['type']=='Linestring':
+            pod['filepath'] = os.path.abspath('fjutrs')
+            mylinestrings.append(pod)
+        elif pod['geometry']['type']=='Polygon':
+            pod['filepath'] = os.path.abspath('fjutrs')
+            mypolygons.append(pod)
+        else:
+            pass
 
-with open("linestrings.geojson","w",encoding="utf-8") as f:
-    json.dump(mylinestrings,f,indent=2,ensure_ascii=False)
 
-with open("polygons.geojson","w",encoding="utf-8") as f:
-    json.dump(mypolygons,f,indent=2,ensure_ascii=False)
+with open("points.geojson","w",encoding="utf-8") as p:
+    json.dump(mypoints,p,indent=2,ensure_ascii=False)
+
+with open("linestrings.geojson","w",encoding="utf-8") as l:
+    json.dump(mylinestrings,l,indent=2,ensure_ascii=False)
+
+with open("polygons.geojson","w",encoding="utf-8") as po:
+    json.dump(mypolygons,po,indent=2,ensure_ascii=False)
