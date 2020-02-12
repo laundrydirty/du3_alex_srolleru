c89cb5c540da98a322738a1328e199af45b20648
laundrydirty <a.sroller@seznam.cz>
2020-02-12 18:04:05 +0100

    validni geojson

--- geocrawler.py
+++ geocrawler.py
@@ -44,18 +44,20 @@ for e in extensions:
 
 try:
     # vystup ve foramtu geojson, features rozdeleny podle geometrie
+    gj_structure_points = {'type': 'FeatureCollection', 'features': mypoints}
     with open("points.geojson", "w", encoding="utf-8") as p:
-        json.dump(mypoints, p, indent=2, ensure_ascii=False)
+        json.dump(gj_structure_points, p, indent=2, ensure_ascii=False)
 
+    gj_structure_lines = {'type': 'FeatureCollection', 'features': mylinestrings}
     with open("lines.geojson", "w", encoding="utf-8") as l:
-        json.dump(mylinestrings, l, indent=2, ensure_ascii=False)
+        json.dump(gj_structure_lines, l, indent=2, ensure_ascii=False)
 
+    gj_structure_polygons = {'type': 'FeatureCollection', 'features': mypolygons}
     with open("polygons.geojson", "w", encoding="utf-8") as po:
-        json.dump(mypolygons, po, indent=2, ensure_ascii=False)
+        json.dump(gj_structure_polygons, po, indent=2, ensure_ascii=False)
+
 #osetreni vyjimek
 except PermissionError:
     print('you do not have write permissions for: ', sys.argv[1])
 except Exception:
     print('NO OUTPUT - unknown error')
-
-
